83c682f0f973e02a3383366ce9b8bf2685044b93
g3n35i5 <janfschmidt@mailbox.org>
2020-01-23 16:41:31 +0100

    User edit view

--- src/models/users/UserEdit.tsx
+++ src/models/users/UserEdit.tsx
@@ -1,15 +1,51 @@
 import React from 'react';
-import { Edit, SimpleForm, BooleanInput, TextInput, ReferenceInput, SelectInput} from 'react-admin';
+import {
+    Edit,
+    SimpleForm,
+    BooleanInput,
+    TextInput,
+    ReferenceInput,
+    SelectInput,
+    PasswordInput,
+    minLength,
+    maxLength,
+    required
+} from 'react-admin';
+
+const customPasswordValidator = (value, allValues) => {
+    if (value && allValues.password_repeat) {
+        if (value !== allValues.password_repeat) {
+            return 'Passwords do not match';
+        }
+    }
+    return [];
+};
+
+const customRepeatPasswordValidator = (value, allValues) => {
+    if (value && allValues.password) {
+        if (value !== allValues.password) {
+            return 'Passwords do not match';
+        }
+    }
+    return [];
+};
+
+const validateFirstName = [minLength(2), maxLength(32)];
+const validateLastName = [required(), minLength(2), maxLength(32)];
+const validatePassword = [minLength(6), customPasswordValidator];
+const validateRepeatPassword = [minLength(6), customRepeatPasswordValidator];
 
 export const UserEdit = props => (
-    <Edit {...props}>
+    <Edit undoable={false} {...props}>
         <SimpleForm>
-            <TextInput source="firstname" />
-            <TextInput source="lastname" />
+            <TextInput source="firstname" validate={validateFirstName}/>
+            <TextInput source="lastname" validate={validateLastName}/>
             <ReferenceInput source="rank_id" reference="ranks">
                 <SelectInput optionText="name" />
             </ReferenceInput>
             <BooleanInput source="is_admin" label="Administrator"/>
+            <PasswordInput source="password" label="Password" validate={validatePassword}/>
+            <PasswordInput source="password_repeat" label="Password repeat" validate={validateRepeatPassword}/>
         </SimpleForm>
     </Edit>
 );
