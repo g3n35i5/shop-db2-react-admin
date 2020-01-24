babf73ab88ddee0e201b15fa7a40155a66cb39b6
g3n35i5 <janfschmidt@mailbox.org>
2020-01-24 10:52:48 +0100

    Adding revoke button to the purchase list

--- src/models/purchases/PurchaseListView.tsx
+++ src/models/purchases/PurchaseListView.tsx
@@ -1,9 +1,45 @@
 import * as React from "react";
-import {NumberField, Datagrid, DateField, List, TextField} from "react-admin";
+import {Datagrid, DateField, List, NumberField, TextField} from "react-admin";
+import {Redo, Undo} from 'mdi-material-ui'
 import UserReferenceField from "../users/UserReferenceField";
 import ProductReferenceField from "../products/ProductReferenceField";
 import DateFieldOptions from "../../shared/options/DateFieldOptions";
 import {CurrencyInCentsField} from '../../shared/fields/CurrencyInCents';
+import { useUpdate, useNotify, useRefresh, Button } from 'react-admin';
+
+
+const TogglePurchaseRevokeButton = ({...props}) => {
+    const notify = useNotify();
+    const refresh = useRefresh();
+
+    let label = props.record.revoked ? 'Undo revoke' : 'Revoke';
+    let message = props.record.revoked ? 'The revoke has been undone' : 'Purchase has been revoked';
+    let icon = props.record.revoked ? <Redo/> : <Undo/>;
+    const [update, { loading, error }] = useUpdate(
+        'purchases',
+        props.record.id,
+        { revoked: !props.record.revoked },
+        props.record,
+        {
+            onSuccess: () => {
+                notify(
+                    message,
+                    'info',
+                    {},
+                    false
+                );
+                refresh()
+            },
+            onFailure: () => {
+                notify(
+                    'Couldn\'t revoke the purchase',
+                    'warning'
+                );
+            },
+        });
+    return <Button label={label} onClick={update} disabled={loading}>{icon}</Button>;
+};
+
 // Mark all revoked purchases with a darker row background
 const purchaseRowStyle = (record, index) => ({
     backgroundColor: record.revoked ? '#212121' : '',
@@ -23,6 +59,7 @@ export const PurchaseList = (props: any) => (
             <NumberField source="amount"/>
             <CurrencyInCentsField source="productprice"/>
             <CurrencyInCentsField source="price"/>
+            <TogglePurchaseRevokeButton/>
         </Datagrid>
     </List>
 );
