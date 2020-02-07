0569f299ddead6bf205858b6b7370113f56e890c
g3n35i5 <janfschmidt@mailbox.org>
2020-02-07 10:45:52 +0100

    CurrencyInCentsField: Fixing NULL values

--- src/shared/fields/CurrencyInCents.tsx
+++ src/shared/fields/CurrencyInCents.tsx
@@ -3,7 +3,7 @@ import React, {FC} from "react";
 import {FieldProps} from "../interfaces/FieldProps";
 
 export const CurrencyInCentsField: FC<FieldProps> = (props: FieldProps) => {
-    if (props.record && props.source) {
+    if (props.record && props.source && props.record[props.source] !== null) {
         // Divide the amount in cents by 100 to get the currency amount in EUR
         const cents = props.record[props.source] / 100;
         return (
