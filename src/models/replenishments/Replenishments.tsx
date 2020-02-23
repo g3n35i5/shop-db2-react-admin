612e64995e9f76bc0500d5e1144ec7b7cb60dc1d
g3n35i5 <janfschmidt@mailbox.org>
2020-02-23 20:15:37 +0100

    Replenishments: Adding interfaces

--- src/models/replenishments/Replenishments.tsx
+++ src/models/replenishments/Replenishments.tsx
@@ -1,4 +1,19 @@
 import { ReplenishmentCollectionList } from "./ReplenishmentCollectionListView";
 import { ReplenishmentCollectionEdit } from "./ReplenishmentCollectionEdit";
+import { ReplenishmentCollectionCreate } from "./ReplenishmentCollectionCreate";
 
-export {ReplenishmentCollectionList, ReplenishmentCollectionEdit};
+export interface Replenishment {
+    id?: number;
+    amount: number;
+    product_id: number;
+    total_price: number
+}
+
+export interface ReplenishmentCollection {
+    id?:number;
+    timestamp: string;
+    comment: string;
+    replenishments: Replenishment[];
+}
+
+export {ReplenishmentCollectionList, ReplenishmentCollectionCreate, ReplenishmentCollectionEdit};
