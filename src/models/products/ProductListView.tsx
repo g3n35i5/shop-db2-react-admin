3e6a2a9e0c3e17d708a76dc9082fd191dd77405e
g3n35i5 <janfschmidt@mailbox.org>
2020-02-24 15:47:56 +0100

    ProductListView: Adding filters and images

--- src/models/products/ProductListView.tsx
+++ src/models/products/ProductListView.tsx
@@ -14,6 +14,18 @@ import {
 } from 'react-admin';
 import {CurrencyInCentsField} from "../../shared/fields/CurrencyInCents";
 import TimestampField from "../../shared/fields/TimestampField";
+import {ProductWithImageField} from "./ProductReferenceField";
+
+
+// Filters for the purchase list view
+const ProductListFilter = (props: any) => (
+    <Filter {...props}>
+        <TextInput source="name" alwaysOn={true}/>
+        <BooleanInput source="active" initialValue={true}/>
+        <BooleanInput source="countable" initialValue={true}/>
+        <BooleanInput source="revocable" initialValue={true}/>
+    </Filter>
+);
 
 
 // Mark all products that are inactive with a darker row background
@@ -24,10 +36,11 @@ const productRowStyle = (record, index) => ({
 export const ProductList = (props: any) => (
     <List
         {...props}
+        filters={<ProductListFilter/>}
         bulkActionButtons={false}>
         <Datagrid rowStyle={productRowStyle} rowClick="edit">
             <NumberField source="id"/>
-            <TextField source="name" />
+            <ProductWithImageField/>
             <CurrencyInCentsField source="price"/>
             <TimestampField source="creation_date"/>
             <TextField source="barcode"/>
