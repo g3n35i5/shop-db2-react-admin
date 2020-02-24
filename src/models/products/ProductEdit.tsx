2abd972146fb75b0c422d28e0672a43f4efb3be7
g3n35i5 <janfschmidt@mailbox.org>
2020-02-24 15:55:54 +0100

    ProductEdit: Making the product price editable

--- src/models/products/ProductEdit.tsx
+++ src/models/products/ProductEdit.tsx
@@ -3,7 +3,7 @@ import {
     BooleanInput,
     Edit,
     ImageField,
-    ImageInput,
+    ImageInput, number, NumberInput,
     ReferenceArrayInput,
     required,
     SelectArrayInput,
@@ -12,9 +12,13 @@ import {
 } from 'react-admin';
 
 export const ProductEdit = props => (
-    <Edit {...props}>
+    <Edit undoable={false} {...props}>
         <SimpleForm>
+            <ImageInput source="imagename" label="New image" accept="image/*">
+                <ImageField source="src" title="title"/>
+            </ImageInput>
             <TextInput source="name" validate={[required()]}/>
+            <NumberInput label="Price in cents" source="price" validate={[number(), required()]}/>
             <TextInput source="barcode"/>
             <BooleanInput source="active" validate={[required()]}/>
             <BooleanInput source="countable" validate={[required()]}/>
