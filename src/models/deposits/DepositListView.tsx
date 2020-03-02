050ec5c95ba8422db8793386ad40fd350f02622d
g3n35i5 <janfschmidt@mailbox.org>
2020-03-02 11:24:21 +0100

    DepositListView: Remove revoke button. Adding filters

--- src/models/deposits/DepositListView.tsx
+++ src/models/deposits/DepositListView.tsx
@@ -1,63 +1,34 @@
 import * as React from "react";
-import {Button, Datagrid, List, NumberField, TextField, useNotify, useRefresh, useUpdate} from "react-admin";
-import {Redo, Undo} from 'mdi-material-ui'
+import {Datagrid, Filter, List, NumberField, TextField, TextInput, BooleanField} from "react-admin";
 import UserReferenceField from "../users/UserReferenceField";
 import {CurrencyInCentsField} from '../../shared/fields/CurrencyInCents';
 import TimestampField from "../../shared/fields/TimestampField";
+import {UserAutoComplete} from "../../shared/fields/UserAutoComplete";
 
-// This component returns a button which toggles the deposit's revoke
-const ToggleDepositRevokeButton = ({...props}) => {
-    const notify = useNotify();
-    const refresh = useRefresh();
-
-    let label = props.record.revoked ? 'Undo revoke' : 'Revoke';
-    let message = props.record.revoked ? 'The revoke has been undone' : 'Deposit has been revoked';
-    let icon = props.record.revoked ? <Redo/> : <Undo/>;
-    const [update, {loading, error}] = useUpdate(
-        'deposits',
-        props.record.id,
-        {revoked: !props.record.revoked},
-        props.record,
-        {
-            onSuccess: () => {
-                notify(
-                    message,
-                    'info',
-                    {},
-                    false
-                );
-                refresh()
-            },
-            onFailure: () => {
-                notify(
-                    'Couldn\'t revoke the deposit',
-                    'warning'
-                );
-            },
-        });
-    return <Button label={label} onClick={update} disabled={loading}>{icon}</Button>;
-};
-
-// Mark all revoked deposits with a darker row background
-const depositRowStyle = (record, index) => ({
-    backgroundColor: record.revoked ? '#212121' : '',
-});
+// Filters for the purchase list view
+const DepositListFilter = (props: any) => (
+    <Filter {...props}>
+        <UserAutoComplete alwaysOn={true}/>
+        <TextInput source="comment" alwaysOn={true}/>
+    </Filter>
+);
 
 // List view for the deposits
 export const DepositList = (props: any) => (
     <List
         {...props}
         bulkActionButtons={false}
+        filters={<DepositListFilter/>}
         sort={{field: 'id', order: 'DESC'}}
     >
-        <Datagrid rowStyle={depositRowStyle}>
+        <Datagrid>
             <NumberField source="id"/>
             <TimestampField source="timestamp"/>
             <UserReferenceField source="admin_id"/>
             <UserReferenceField source="user_id"/>
             <CurrencyInCentsField source="amount"/>
             <TextField source="comment"/>
-            <ToggleDepositRevokeButton/>
+            <BooleanField source="revoked"/>
         </Datagrid>
     </List>
 );
